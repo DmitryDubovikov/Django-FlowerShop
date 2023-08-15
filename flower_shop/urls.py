95ea4e49fdb0510940178593310161a03b27de6d
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-08-15 20:31:14 -0300

    connect catalog.html page

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+
+from .views import catalog, index
 
 app_name = "flower_shop"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("catalog/", catalog, name="catalog"),
 ]
