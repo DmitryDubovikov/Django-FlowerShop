0d8fd4069338a256ba0602151f576639aa453fd9
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-08-15 23:29:02 -0300

    add bouquets to catalog.html

--- flower_shop/views.py
+++ flower_shop/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from .models import Bouquet
+
 
 def index(request):
     return render(request, "flower_shop/index.html")
 
 
 def catalog(request):
-    return render(request, "flower_shop/catalog.html")
+    context = {"bouquets": Bouquet.objects.all()}
+    return render(request, "flower_shop/catalog.html", context)
