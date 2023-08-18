d08d51e8d8d70f2d8c418e1b2383892af51b3847
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-08-18 10:26:08 -0300

    add total_sum field to order

--- flower_shop/views.py
+++ flower_shop/views.py
@@ -68,12 +68,14 @@ def order(request, bouquet_id):
     if request.method == "GET":
         try:
             validate_international_phonenumber(request.GET["tel"])
+            bouquet = Bouquet.objects.get(id=bouquet_id)
             new_order = Order.objects.create(
-                bouquet=Bouquet.objects.get(id=bouquet_id),
+                bouquet=bouquet,
                 name=request.GET["fname"],
                 phone=request.GET["tel"],
                 address=request.GET["adres"],
                 preferred_delivery_time=request.GET["orderTime"],
+                total_sum=bouquet.price,
             )
             context["order_created"] = True
             context["order_id"] = new_order.id
